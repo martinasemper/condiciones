3251f8e8d9f14932ddf9dd13a051226c27b52107
martinasemper <136343432+martinasemper@users.noreply.github.com>
2023-10-27 21:10:49 -0300

    Add files via upload

---
+++ condiciones js/condiciones.js
@@ -0,0 +1,43 @@
+nombre =prompt('nombre')
+    alert('bienvenido nahuel')
+2
+if (eda>=18){
+    alert('sos mayor')
+}else (edad<=10) ('no sos mayor')
+
+3 
+ if(edad<=11){
+    alert('sos un niño')
+}else (edad<=18)
+    alert('sos un adolecente')
+
+
+if(edad<=26){
+    alert('sos un joven')
+}else(edad<=59)
+    alert('sos un adulto')
+
+
+if(edad<=1100){
+    alert('sos un anciano')
+}
+4
+ diaSemana=prompt("por favor ingrese un dia de la semana");
+ alert('viernes')
+ console.log("el dia de la semana es viernes" + diaSemana)
+ alert('dia laborable')
+5
+contraseña=prompt("Por favor ingrese su contraseña");
+if(contraseña==="secreto"){
+    console.log("acceso concedido")
+}else {
+    console.log("contraseña incorrecta acceso denegado")
+}
+6
+numero=15
+if(esMultiplode5(15)){
+    console.log(15+"es multiplo de 5")
+}
+
+
+
